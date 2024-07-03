6abb1d88da691523ef9d77830d27744f20646203
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-07-03 00:10:25 -0300

    feat(src/app): register missing routes and jwt middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
-import { appRoutes } from '@/http/routes'
 import { ZodError } from 'zod'
 import { env } from '@/env'
+import { usersRoutes } from '@/http/controllers/users/routes'
+import { gymsRoutes } from '@/http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
-app.register(appRoutes)
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -17,7 +26,7 @@ app.setErrorHandler((error, _, reply) => {
   if (env.NODE_ENV !== 'production') {
     console.error(error)
   } else {
-    // TODO: log to a external tool like DataDog, NewRelic or Sentry
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
   return reply.status(500).send({ message: 'Internal server error.' })
